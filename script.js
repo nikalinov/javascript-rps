cfee2eeeba5fac08367eff855903d775d0229c85
nikalinov <nikitakalinovskij@gmail.com>
2023-12-23 18:24:09 +0000

    Made bo5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
-function announceWinner(choice, compChoice) {
+function defineWinner(choice, compChoice) {
     choice = choice.toUpperCase();
     compChoice = compChoice.toUpperCase();
 
@@ -8,19 +8,39 @@ function announceWinner(choice, compChoice) {
     if ((choice === "ROCK" && compChoice === "PAPER") ||
         (choice === "PAPER" && compChoice === "SCISSORS") ||
         (choice === "SCISSORS" && compChoice === "ROCK")) {
-        result += "You lost!";
+        return "computer";
     } else if (choice === compChoice) {
-        result += "Tie!";
+        return "tie";
     } else {
-        result += "You won!";
+        return "player";
     }
-    return result;
 }
 
-let choice = prompt(`Input ${choices}, or QUIT:`, "QUIT").toUpperCase();
+let choice = "";
 while (choice !== "QUIT") {
-    let compChoice = () => choices[Math.floor(Math.random() * choices.length)].toLowerCase();
-    let result = announceWinner(choice, compChoice());
-    alert(result);
-    choice = prompt(`Input ${choices}, or QUIT:`, "QUIT").toUpperCase();
+    let scores = {player: 0, computer: 0};
+
+    while (scores.player <= 2 && scores.computer <= 2) {
+        choice = prompt(`Input ${choices}, or QUIT:`, "QUIT").toUpperCase();
+        if (choice === "QUIT") {
+            break;
+        }
+        let defineCompChoice = () => choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+        let compChoice = defineCompChoice();
+        let result = defineWinner(choice, compChoice);
+        let message = "";
+
+        if (result === "player") {
+            scores.player += 1;
+            message += "You won this one!";
+        } else if (result === "computer") {
+            scores.computer += 1;
+            message += "You lost this one :( ";
+        } else {
+            message += "Tie! "
+        }
+
+        message += `Computer's choice: ${compChoice}. Score: ${scores.player} (you) : ${scores.computer} (computer)`;
+        alert(message);
+    }
 }
