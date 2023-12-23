d824ca9d87953851710713f59ea9e724d60fe169
nikalinov <nikitakalinovskij@gmail.com>
2023-12-23 17:54:50 +0000

    Main functionality

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
-let choice = prompt(`Input ${choices}, or QUIT:`, "QUIT");
-while (choice.toUpperCase() !== "QUIT") {
-    let compChoice = () => choices[Math.floor(Math.random() * choices.length)];
+function announceWinner(choice, compChoice) {
+    choice = choice.toUpperCase();
+    compChoice = compChoice.toUpperCase();
 
+    let result = `The opponent's choice: ${compChoice}. `;
+    if ((choice === "ROCK" && compChoice === "PAPER") ||
+        (choice === "PAPER" && compChoice === "SCISSORS") ||
+        (choice === "SCISSORS" && compChoice === "ROCK")) {
+        result += "You lost!";
+    } else if (choice === compChoice) {
+        result += "Tie!";
+    } else {
+        result += "You won!";
+    }
+    return result;
+}
+
+let choice = prompt(`Input ${choices}, or QUIT:`, "QUIT").toUpperCase();
+while (choice !== "QUIT") {
+    let compChoice = () => choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+    let result = announceWinner(choice, compChoice());
+    alert(result);
+    choice = prompt(`Input ${choices}, or QUIT:`, "QUIT").toUpperCase();
 }
